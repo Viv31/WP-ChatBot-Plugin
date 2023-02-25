02b147a4a316960872073c2c498dd097200f0baf
vaibhav <vivgangs@gmail.com>
2023-02-26 00:25:38 +0530

    Added db tables of chatbot

---
+++ db/chatbot_hints.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 25, 2023 at 07:54 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wordpress_learning`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatbot_hints`
+--
+
+CREATE TABLE `chatbot_hints` (
+  `id` int(11) NOT NULL,
+  `question` varchar(100) NOT NULL,
+  `reply` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `chatbot_hints`
+--
+
+INSERT INTO `chatbot_hints` (`id`, `question`, `reply`) VALUES
+(1, 'HI||Hello||Hola', 'Hello, how are you.'),
+(2, 'How are you', 'Good to see you again!'),
+(3, 'what is your name||whats your name', 'My name is Vishal Bot'),
+(4, 'what should I call you', 'You can call me Vishal Bot'),
+(5, 'Where are your from', 'I m from India'),
+(6, 'Bye||See you later||Have a Good Day', 'Sad to see you are going. Have a nice day');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chatbot_hints`
+--
+ALTER TABLE `chatbot_hints`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chatbot_hints`
+--
+ALTER TABLE `chatbot_hints`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
